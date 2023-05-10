fe5e87b61237dc1cb70e21b5200845f0a56abfa9
Grenui92 <grenui92@gmail.com>
2023-05-10 23:26:35 +0300

    new models.py

--- personal_assistant/storageapp/models.py
+++ personal_assistant/storageapp/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class FileTypes(models.Model):
+
+    name = models.CharField(max_length=255)
+
+class FileExtensions(models.Model):
+
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(FileTypes, on_delete=models.CASCADE, default=1)
+
+class File(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    file_type = models.ForeignKey(FileTypes, on_delete=models.CASCADE)
+    file_extension = models.ForeignKey(FileExtensions, on_delete=models.CASCADE)
+    file_name = models.CharField(max_length=255, blank=False, default='default_name')
+    file_url = models.CharField()
