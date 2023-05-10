e21196b9c63fabd75cc731568aa8048290d18575
PIRomanCod <111997187+PIRomanCod@users.noreply.github.com>
2023-05-10 21:26:10 +0300

    first version of auth and password reset

--- personal_assistant/users/models.py
+++ personal_assistant/users/models.py
@@ -1,3 +1,24 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+# Extending User Model Using a One-To-One Link
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default_avatar.png', upload_to='profile_images')
+
+    def __str__(self):
+        return self.user.username
+
+    # resizing images
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 250 or img.width > 250:
+            new_img = (250, 250)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
--- personal_assistant/users/views.py
+++ personal_assistant/users/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import RegisterForm, LoginForm, ProfileForm
+
+
+def signupuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='contactapp:main')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='contactapp:main')
+        else:
+            return render(request, 'users/signup.html', context={"form": form})
+
+    return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect('main')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='contactapp:main')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
+
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='contactapp:main')
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users:profile')
+
+    profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'users/profile.html', {'profile_form': profile_form})
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
