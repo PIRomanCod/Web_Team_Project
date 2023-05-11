1d28dd4936251ba2d1a3961ba3bd99dd6297565d
PIRomanCod <111997187+PIRomanCod@users.noreply.github.com>
2023-05-11 19:29:36 +0300

    integrated user actions + note create to main page

--- personal_assistant/personal_assistant/urls.py
+++ personal_assistant/personal_assistant/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', include('newsapp.urls')),
     path('notes/', include('noteapp.urls')),
     path('users/', include('users.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
