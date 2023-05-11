e9a62fc073f9f41789f8bf94f94f5182f326fd5b
PIRomanCod <111997187+PIRomanCod@users.noreply.github.com>
2023-05-11 19:20:41 +0300

    integrated user actions + note create to main page

--- personal_assistant/personal_assistant/urls.py
+++ personal_assistant/personal_assistant/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-feature-base-html
     path('', include('newsapp.urls')),
     path('notes/', include('noteapp.urls')),
     path('users/', include('users.urls')),
