1eb719eda7e55dbf4adc25a2ed770bf1caa61967
Grenui92 <grenui92@gmail.com>
2023-05-12 23:10:09 +0300

    add new columns to sort files by date

--- personal_assistant/storageapp/models.py
+++ personal_assistant/storageapp/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 class FileTypes(models.Model):
 
@@ -15,5 +16,8 @@ class File(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     file_type = models.ForeignKey(FileTypes, on_delete=models.CASCADE)
     file_extension = models.ForeignKey(FileExtensions, on_delete=models.CASCADE)
-    file_name = models.CharField(max_length=255, blank=False, default='default_name')
-    file_url = models.CharField()
+    file_name = models.CharField(max_length=255)
+    dropbox_file_name = models.CharField(max_length=255, null=True)
+    file_url = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
