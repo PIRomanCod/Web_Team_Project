50b46a1b8adce4174b85f90d971762f2fa636db4
Jymbarbaruoky <116517553+Jymbarbaruoky@users.noreply.github.com>
2023-05-17 02:36:22 +0300

    updated
    виправив зауваження по пунктах

--- personal_assistant/contactapp/forms.py
+++ personal_assistant/contactapp/forms.py
@@ -3,6 +3,12 @@ from .models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    name = forms.CharField(required=True)
+    phone_number = forms.CharField(required=True)
+    birth_date = forms.DateField(required=False, widget=forms.DateInput(format='%Y-%m-%d',
+                                                                        attrs={'placeholder': 'year-mount-day'})
+    )
+
     class Meta:
         model = Contact
         fields = ['name', 'address', 'phone_number', 'email', 'birth_date']
--- personal_assistant/contactapp/models.py
+++ personal_assistant/contactapp/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=20)
-    email = models.EmailField()
-    birth_date = models.DateField()
+    address = models.CharField(max_length=255, blank=True)
+    phone_regex = RegexValidator(r'^\d{5,15}$', 'Enter a valid phone number.'
+                                                ' Min lenght is 5, max lenght is 15, only numbers.')
+    phone_number = models.CharField(max_length=15, validators=[phone_regex])
+    email = models.EmailField(blank=True)
+    birth_date = models.DateField(blank=True, null=True, default=None)
 
     def __str__(self):
         return self.name
