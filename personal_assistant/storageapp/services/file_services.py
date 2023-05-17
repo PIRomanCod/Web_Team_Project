dbe64c3acb0f0c683e9748007d2dfeec172f6445
Grenui92 <grenui92@gmail.com>
2023-05-17 21:51:53 +0300

    imply search results view to the render_files_list

--- personal_assistant/storageapp/services/file_services.py
+++ personal_assistant/storageapp/services/file_services.py
@@ -6,7 +6,6 @@ from storages.backends.dropbox import DropBoxStorage
 from storageapp.models import File, FileTypes, FileExtensions
 
 
-
 class FileServices:
     reg_ex_existance = r'\.[^./\\]+$'
     dbx_storage = DropBoxStorage()
@@ -36,7 +35,7 @@ class FileServices:
     @classmethod
     def get_file_info(cls, request):
         file = request.FILES.get('file')
-        owner_inst: User = User.objects.get(id=request.user.id)
+        owner_inst = User.objects.get(id=request.user.id)
         user_file_name = request.POST.get('user_input')
 
         try:
@@ -60,7 +59,6 @@ class FileServices:
 
         dropbox_file_name = cls.dbx_storage.save(file.name, file)
         return dropbox_file_name
-
 
     @classmethod
     def delete_file(cls, file):
@@ -103,9 +101,10 @@ class FileServices:
                     FileExtensions.objects.create(name=ext, category=inst)
 
     @staticmethod
-    def render_files_list(request):
+    def render_files_list(request, files_list=None, message='This is your files.'):
+
         fields_to_order = File.get_fields_list()
-        fields_to_choice = {2: 'Type', 3: 'Extension', 4: 'File Name',  6: 'Created at'}
+        fields_to_choice = {2: 'Type', 3: 'Extension', 4: 'File Name', 6: 'Created at'}
 
         all_files_types = [type.name for type in FileTypes.objects.all()]
 
@@ -113,9 +112,17 @@ class FileServices:
             files_types_enabled = all_files_types
         else:
             files_types_enabled = request.GET.getlist('filter_type')
+
         how_order = int(request.GET.get('category')) if request.GET.get('category') else 0
         reverse_order = '-' if '-' in files_types_enabled else ''
         files_types_obj = [FileTypes.objects.get(name=name) for name in files_types_enabled if name != '-']
+
+        if files_list:
+            return render(request, 'storageapp/files_list.html', context={'files_list': files_list,
+                                                                          'files_types_enabled': files_types_enabled,
+                                                                          'file_fields': fields_to_choice,
+                                                                          'all_files_types': all_files_types,
+                                                                          'message': message})
         files_list = (File.objects.filter(owner=request.user.id)
                       .filter(file_type__in=files_types_obj)
                       .order_by(f'{reverse_order}{fields_to_order[how_order]}'))
@@ -123,5 +130,5 @@ class FileServices:
         return render(request, 'storageapp/files_list.html', context={'files_list': files_list,
                                                                       'files_types_enabled': files_types_enabled,
                                                                       'file_fields': fields_to_choice,
-                                                                      'all_files_types': all_files_types})
-
+                                                                      'all_files_types': all_files_types,
+                                                                      'message': message})
